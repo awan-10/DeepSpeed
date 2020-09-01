135864eb87e7554db7d5323c92e0f2f3567fb2ce
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2020-09-01 01:04:50 +0000

    Update the test_com_reduce

--- deepspeed/pt/test_com_reduce.py
+++ deepspeed/pt/test_com_reduce.py
@@ -22,6 +22,8 @@ def torch_sim(a):
     #a_sign = a.sign()
     scale = a.norm() / np.sqrt(a.numel())
     a_compressed = scale * a_sign
+    a_sign = None
+    worker_error = a - a_compressed
     dist.all_reduce(a_compressed)
     a_compressed.mul_(1/dist.get_world_size())
     a_server_sign = a_compressed.sign().add_(1).bool().float().add_(-0.5).mul_(2.0)
@@ -30,27 +32,32 @@ def torch_sim(a):
     server_scale = [chunk_a.norm()/np.sqrt(chunk_a.numel()) for chunk_a in a_list]
     a_sign_list = torch.chunk(a_server_sign,dist.get_world_size())
     a_server_compressed = torch.cat([ server_scale[i] * a_sign_list[i] for i in range(dist.get_world_size()) ])
-    return a_server_compressed
+    rank = dist.get_rank()
+    server_error = a_list[rank] - server_scale[rank] * a_sign_list[rank]
+    return a_server_compressed, worker_error, server_error
 
-tensor_size =  400 * 2**20
+tensor_size =  200 * 2**20
 server_size = int(tensor_size/size)
 
 # a = -torch.ones(tensor_size, device=device)
-a = torch.randn(tensor_size, device=device)
+a = torch.randn(tensor_size, device=device) 
 #if rank == 0:
  #   print('a is: ',a)
 worker_error = torch.zeros_like(a)
 server_error = torch.zeros(server_size, device=device)
-a_torch = torch_sim(a)
-
+a_torch, worker_error_torch, server_error_torch = torch_sim(a)
+torch.cuda.empty_cache()
 
 a_after = dummy_optim.Compressed_Allreduce(a, worker_error, server_error, rank, size, comm)
 #print('a becomes ',a)
-if rank == 0:
+#if rank == 0:
+if True:
+    print('Rank is {} =============================================='.format(rank))
     print('Diff is: ', torch.norm(a_after - a_torch))
     #print('Original Norm is: ', torch.norm(a_after))
     #print('Compressed_addreduce gives: ', a_after[0:10])
-    #print('Worker error is: ',worker_error[0:10])
-    #print('Server error is: ',server_error[0:10] )
+    print('Worker error diff is: ',torch.norm(worker_error - worker_error_torch))
+    print('Server error is: ', torch.norm(server_error - server_error_torch))
+    print('+++++++++++++++++++++++++++++++')
     #print('torch sim gives: ', a_torch[0:10])
 
